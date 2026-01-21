bd7f55792ca0c997db2833c526b292ac45e6efc5
Hidde Stokvis <hidde@neverhide.nl>
2026-01-21 10:27:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
     name="target-duckdb",
-    version="0.8.1",
+    version="0.8.2",
     description="Singer.io target for loading data into DuckDB",
     long_description=long_description,
     long_description_content_type="text/markdown",
